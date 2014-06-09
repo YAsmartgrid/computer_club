9c407e3e0a5a34e01079dc3871d6d97fe8c6ff27
Yu Asabe <lajolla2005@gmail.com>
2014-06-09 20:46:30 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ComputerClub::Application.config.secret_key_base = '09a3b68e69dd22af68ad18a02a701ce356bd8a5de21e1ce54b61e7f00734d86e267bfa0bb4ca860a96d5ca2faa29530532e286257702f167573a3210ff9b6777'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ComputerClub::Application.config.session_store :cookie_store, key: '_computer_club_session'
